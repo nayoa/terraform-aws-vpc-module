3f4b2c3d0d18339a10e270fc1e994e48fbf36015
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-05-21 15:11:00 +0100

    First commit

---
+++ variables.tf
@@ -0,0 +1,118 @@
+// variable "environment" {
+//   type        = "string"
+//   default     = "dev"
+//   description = "The environment the infrastructure is provisioned in. e.g. dev, qa, uat and prod"
+// }
+
+variable "region" {
+  type        = "string"
+  default     = "eu-west-1"
+  description = "The AWS region for the infrastructure to be provisioned in"
+}
+
+variable "name" {
+  type        = "string"
+  default     = ""
+  description = "Name to be used on all the resources as identifier"
+}
+
+variable "resource_tags" {
+  type        = "map"
+  default     = {}
+  description = "A mapping of tags to assign to the networking resources"
+}
+
+variable "vpc_cidr" {
+  type        = "string"
+  description = "The CIDR block for the VPC."
+}
+
+variable "enable_dns_support" {
+  type        = "string"
+  default     = "true"
+  description = "A boolean flag to enable/disable DNS support in the VPC"
+}
+
+variable "enable_dns_hostnames" {
+  type        = "string"
+  default     = "false"
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC"
+}
+
+variable "assign_generated_ipv6_cidr_block" {
+  type        = "string"
+  default     = "false"
+  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block"
+}
+
+variable "secondary_cidr_blocks" {
+  default     = []
+  description = "List of secondary CIDR blocks to associate with the VPC to extend the IP Address pool"
+}
+
+variable "public_subnets" {
+  default     = []
+  description = "A list of public subnets inside the VPC"
+}
+
+variable "private_subnets" {
+  default     = []
+  description = "A list of private subnets inside the VPC"
+}
+
+variable "one_nat_gateway_per_az" {
+  type        = "string"
+  default     = false
+  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`"
+}
+
+variable "create_database_subnet_group" {
+  type        = "string"
+  default     = true
+  description = "Controls if database subnet group should be created"
+}
+
+variable "database_subnets" {
+  default     = []
+  description = "A list of database subnets"
+}
+
+variable "create_database_subnet_route_table" {
+  default     = false
+  description = "Controls if separate route table for database should be created"
+}
+
+variable "create_database_nat_gateway_route" {
+  default     = false
+  description = "Controls if a nat gateway route should be created to give internet access to the database subnets"
+}
+
+variable "azs" {
+  default     = []
+  description = "A list of availability zones in the region"
+}
+
+variable "create_database_internet_gateway_route" {
+  default     = false
+  description = "Controls if an internet gateway route for public database access should be created"
+}
+
+variable "enable_public_nat_gateway" {
+  default     = false
+  description = "Should be true if you want to provision NAT Gateways for each of your public networks"
+}
+
+variable "enable_private_nat_gateway" {
+  default     = false
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  default     = false
+}
+
+variable "map_public_ip_on_launch" {
+  default     = false
+  description = "Specify true to indicate that instances launched into the subnet should be assigned a public IP address"
+}
---
+++ vpc.tf
@@ -0,0 +1,156 @@
+### VPC
+
+resource "aws_vpc" "main" {
+  cidr_block                       = "${var.vpc_cidr}"
+  enable_dns_hostnames             = "${var.enable_dns_hostnames}"
+  enable_dns_support               = "${var.enable_dns_support}"
+  assign_generated_ipv6_cidr_block = "${var.assign_generated_ipv6_cidr_block}"
+
+  tags = "${var.resource_tags}"
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
+  count = "${length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0}"
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  cidr_block = "${element(var.secondary_cidr_blocks, count.index)}"
+}
+
+### Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = "${var.resource_tags}"
+}
+
+### Public Routing
+
+resource "aws_route_table" "public" {
+  count = "${length(var.public_subnets)}"
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = "${var.resource_tags}"
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count = "${length(var.public_subnets)}"
+
+  route_table_id         = "${element(aws_route_table.public.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
+### Private Routing
+
+resource "aws_route_table" "private" {
+  count = "${local.max_subnet_length > 0 ? local.nat_gateway_count : 0}"
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = "${var.resource_tags}"
+}
+
+### Database Routes
+
+resource "aws_route_table" "database" {
+  count = "${var.create_database_subnet_route_table && length(var.database_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = "${var.resource_tags}"
+}
+
+resource "aws_route" "database_internet_gateway" {
+  count = "${var.create_database_subnet_route_table && length(var.database_subnets) > 0 && var.create_database_internet_gateway_route && !var.create_database_nat_gateway_route ? 1 : 0}"
+
+  route_table_id         = "${aws_route_table.database.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
+resource "aws_route" "database_nat_gateway" {
+  count = "${var.create_database_subnet_route_table && length(var.database_subnets) > 0 && !var.create_database_internet_gateway_route && var.create_database_nat_gateway_route && var.enable_private_nat_gateway ? local.nat_gateway_count : 0}"
+
+  route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${element(aws_nat_gateway.gw.*.id, count.index)}"
+}
+
+### Public Subnet
+
+resource "aws_subnet" "public" {
+  count = "${length(var.public_subnets) > 0 && (!var.one_nat_gateway_per_az || length(var.public_subnets) >= length(var.azs)) ? length(var.public_subnets) : 0}"
+
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "${element(concat(var.public_subnets, list("")), count.index)}"
+  availability_zone       = "${element(var.azs, count.index)}"
+  map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
+
+  tags = "${var.resource_tags}"
+}
+
+### Private Subnet
+
+resource "aws_subnet" "private" {
+  count = "${length(var.private_subnets) > 0 ? length(var.private_subnets) : 0}"
+
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${var.private_subnets[count.index]}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags = "${var.resource_tags}"
+}
+
+### Database Subnet
+
+resource "aws_subnet" "database" {
+  count = "${length(var.database_subnets) > 0 ? length(var.database_subnets) : 0}"
+
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${var.database_subnets[count.index]}"
+  availability_zone = "${element(var.azs, count.index)}"
+}
+
+resource "aws_db_subnet_group" "database" {
+  count = "${length(var.database_subnets) > 0 && var.create_database_subnet_group ? 1 : 0}"
+
+  name        = "${lower(var.name)}"
+  description = "Database subnet group for ${var.name}"
+  subnet_ids  = ["${aws_subnet.database.*.id}"]
+
+  tags = "${var.resource_tags}"
+}
+
+### Nat Gateway
+
+resource "aws_eip" "nat" {
+  count = "${var.enable_public_nat_gateway ? local.nat_gateway_count : 0}"
+  vpc   = true
+
+  tags = "${var.resource_tags}"
+}
+
+resource "aws_nat_gateway" "gw" {
+  count         = "${var.enable_public_nat_gateway ? local.nat_gateway_count : 0}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
+
+  tags = "${var.resource_tags}"
+
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count = "${var.enable_private_nat_gateway ? local.nat_gateway_count : 0}"
+
+  route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${element(aws_nat_gateway.gw.*.id, count.index)}"
+
+  timeouts {
+    create = "5m"
+  }
+}
