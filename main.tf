2728e85ddbebf74c2cf72f943d85e223f71895ac
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-05-23 16:09:33 +0100

    PTS-2636: Remove git validate

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "eve"
-  }
-}
-
 locals {
   max_subnet_length = "${max(length(var.private_subnets), length(var.database_subnets))}"
   nat_gateway_count = "${var.single_private_nat_gateway ? 1 : (var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length)}"
