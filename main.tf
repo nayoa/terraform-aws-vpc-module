2d26ca141ad255573f554fb5f37a12e788a56129
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-06-12 14:45:57 +0100

    PTS-2636: Add unique naming convention to autoscaling group and launch configuration

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ locals {
   max_subnet_length = "${max(length(var.private_subnets), length(var.database_subnets))}"
   nat_gateway_count = "${var.single_private_nat_gateway ? 1 : (var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length)}"
 }
+
+resource "random_string" "launch_configuration" {
+  length = 5
+}
+
+resource "random_string" "autoscaling_group" {
+  length = 5
+}
