4226ed59503c7202168c408bdae80a54c13a778e
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-06-13 18:20:22 +0100

    PTS-2636: Render user data template

--- bastion.tf
+++ bastion.tf
@@ -18,19 +18,12 @@ resource "aws_launch_configuration" "bastion" {
   image_id                    = "${data.aws_ami.hardened_ami.id}"
   instance_type               = "${var.bastion_instance_type}"
   security_groups             = [aws_security_group.bastion.id]
-  user_data                   = file("${path.module}/templates/user_data.sh")
+  user_data                   = templatefile("${path.module}/templates/user_data.sh.tpl", environment = "${local.environment}")
   associate_public_ip_address = true
 
   lifecycle {
     create_before_destroy = true
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "export ENVIRONMENT=${local.environment}"
-    ]
-  }
-}
 
 ### Autoscaling group
 
--- templates/user_data.sh.tpl
+++ templates/user_data.sh.tpl
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# shellcheck disable=SC2154
+
 # Update the OS to begin with to catch up on the latest packages.
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Add hostname for bastion host
-sudo hostnamectl set-hostname bastion-"$ENVIRONMENT"
-sudo sed 's|localhost|$ENVIRONMENT|g' /etc/hosts
+sudo hostnamectl set-hostname bastion-"${environment}"
+sudo sed 's|localhost|"${environment}"|g' /etc/hosts
 sudo reboot
