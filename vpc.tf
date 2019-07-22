95b04b3bbf51cd3ef7ef923c3b4bada81c0d9e86
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-07-22 12:18:28 +0100

    PTS-2636: Fix outputs

--- vpc.tf
+++ vpc.tf
@@ -94,7 +94,7 @@ resource "aws_db_subnet_group" "database" {
 
   name        = lower(var.name)
   description = "Database subnet group for ${local.environment}"
-  subnet_ids  = ["${aws_subnet.private[*].id}"]
+  subnet_ids  = aws_subnet.private.*.id
 
   tags = merge(var.tags, map("Name", "Database Subnet Group (${local.environment})"))
 }
